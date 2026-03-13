4374ca18769c8abe684c2f959c8a923dad610751
asaassystem <asaas.system@gmail.com>
2026-03-13 09:07:22 +0300

    Fix: NjmApp - remove SessionManager.init() call

--- app/src/main/java/com/njm/worker/NjmApp.kt
+++ app/src/main/java/com/njm/worker/NjmApp.kt
@@ -1,11 +1,9 @@
 package com.njm.worker
 
 import android.app.Application
-import com.njm.worker.utils.SessionManager
 
 class NjmApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        SessionManager.init(this)
     }
 }
