3a35be87cc435da15cb8e2985b9fe793b0471149
asaassystem <asaas.system@gmail.com>
2026-03-14 19:42:48 +0300

    Design v2: New UI + Sunmi printer fix + NjmApp improved

--- app/src/main/java/com/njm/worker/NjmApp.kt
+++ app/src/main/java/com/njm/worker/NjmApp.kt
@@ -8,50 +8,52 @@ import com.sunmi.peripheral.printer.InnerPrinterManager
 import com.sunmi.peripheral.printer.SunmiPrinterService
 
 /**
- * NJM Application class - binds Sunmi printer service on startup
-  * Developer: meshari.tech
-   */
+ * NJM Worker Application
+ * Binds Sunmi built-in printer on startup via official InnerPrinterManager SDK
+ * Developer: meshari.tech
+ */
 class NjmApp : Application() {
 
-        companion object {
-                    lateinit var instance: NjmApp
-                        private set
-                    var sunmiPrinter: SunmiPrinterService? = null
-                    var printerConnected: Boolean = false
-                    var bindAttempted: Boolean = false
-        }
+    companion object {
+        lateinit var instance: NjmApp
+            private set
+        var sunmiPrinter: SunmiPrinterService? = null
+        var printerConnected: Boolean = false
+        var bindAttempted: Boolean = false
+    }
 
-            override fun onCreate() {
-                        super.onCreate()
-                                instance = this
-                        bindSunmiPrinter()
-            }
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        bindSunmiPrinter()
+    }
 
-                private fun bindSunmiPrinter() {
-                            try {
-                                            bindAttempted = true
-                                            InnerPrinterManager.getInstance().bindService(
-                                                                this,
-                                                                object : InnerPrinterCallback() {
-                                                                                        override fun onConnected(service: SunmiPrinterService?) {
-                                                                                                                    sunmiPrinter = service
-                                                                                                                    printerConnected = true
-                                                                                                                    Log.d("NJM_Printer", "Sunmi printer connected in Application")
-                                                                                        }
-
-                                                                                                            override fun onDisconnected() {
-                                                                                                                                        sunmiPrinter = null
-                                                                                                                                        printerConnected = false
-                                                                                                                                        Log.w("NJM_Printer", "Sunmi printer disconnected")
-                                                                                                            }
-                                                                }
-                                                                            )
-                            } catch (e: InnerPrinterException) {
-                                            Log.w("NJM_Printer", "Not a Sunmi device or SDK error: ${e.message}")
-                                                        printerConnected = false
-                            } catch (e: Exception) {
-                                            Log.w("NJM_Printer", "Printer bind error: ${e.message}")
-                                                        printerConnected = false
-                            }
+    private fun bindSunmiPrinter() {
+        if (bindAttempted) return
+        try {
+            bindAttempted = true
+            InnerPrinterManager.getInstance().bindService(
+                this,
+                object : InnerPrinterCallback() {
+                    override fun onConnected(service: SunmiPrinterService?) {
+                        sunmiPrinter = service
+                        printerConnected = (service != null)
+                        Log.i("NJM_Printer", "Sunmi printer connected")
+                    }
+                    override fun onDisconnected() {
+                        sunmiPrinter = null
+                        printerConnected = false
+                        bindAttempted = false
+                        Log.w("NJM_Printer", "Sunmi printer disconnected")
+                    }
                 }
+            )
+        } catch (e: InnerPrinterException) {
+            Log.w("NJM_Printer", "Not a Sunmi device: ${e.message}")
+            printerConnected = false
+        } catch (e: Exception) {
+            Log.w("NJM_Printer", "Printer bind error: ${e.message}")
+            printerConnected = false
+        }
+    }
 }
